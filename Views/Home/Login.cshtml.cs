b098ac04cda8f2ba89fc95315c879181f1cb2901
Alizon2604 <penalozanoriegaa@gmail.com>
2025-04-05 13:39:56 -0700

    Se agrego login

---
+++ Views/Home/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistemas_de_inventario.Views.Home
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
